138ec74bdce9808cc4568b05ca3bc2330141446d
annechege <annechege780@gmail.com>
2021-05-26 12:10:39 +0300

    modifying repository and form-display component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { from } from 'rxjs';
+import{FormDispalyComponent} from './form-dispaly/form-dispaly.component'
+import{RepositoryComponent} from './repository/repository.component'
+const routes: Routes = [
+{ path: 'form-display', component: FormDispalyComponent},
+{ path: 'repository', component: RepositoryComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -11,61 +11,6 @@ import {ReposService} from '../repos-http/repos.service'
 })
 export class RepositoryComponent implements OnInit {
 
-  repo: Repository;
-  userName: string;
-  repoName: string;
-
-
-  constructor(private http:HttpClient, private repoRequestService: ReposService){
-    this.repo = new Repository("","","","","");
-  }
-
-    repoRequest(){
-
-      interface ApiResponse{
-        html_url: any;
-        name: string;
-        description: any;
-        forks: number;
-        license: any;
-      }
-      let promise = new Promise((resolve,reject)=>{
-        this.http.get<ApiResponse>("https://api.github.com/repos/" + this.userName + "/" + this.repoName).toPromise().then(response=>{
-
-          this.repo.link = response.html_url
-          this.repo.name = response.name
-          this.repo.description
-           = response.description
-          this.repo.forks = response.forks
-          this.repo.license = response.license
-
-        resolve(response);
-
-      },
-      error=>{
-
-        this.repo.link = ""
-        this.repo.name = ""
-        this.repo.description
-         = ""
-        this.repo.forks = ""
-        this.repo.license = ""
-
-        reject(error);
-
-
-        })
-      })
-
-      return promise
-
-    }
-
-    repoLookup(){
-      this.repoRequestService.repoLookup(this.userName);
-      this.repoRequestService.getRepoData();
-      this.repo = this.repoRequestService.repos;
-    }
     ngOnInit() {
 
     }
