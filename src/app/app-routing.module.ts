03650c42f42bf8c42d6838baa1495603ef231bbc
annechege <annechege780@gmail.com>
2021-05-26 12:49:13 +0300

    styling the nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { from } from 'rxjs';
 import{FormDispalyComponent} from './form-dispaly/form-dispaly.component'
 import{RepositoryComponent} from './repository/repository.component'
-
+import{NavBarComponent} from './nav-bar/nav-bar.component'
 
 const routes: Routes = [
 { path: 'form-display', component: FormDispalyComponent},
