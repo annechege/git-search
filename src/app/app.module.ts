e65a4d5b5bbe0a1a2a5d14ff4242bd9db666cd0c
annechege <annechege780@gmail.com>
2021-05-27 11:09:48 +0300

    deploying the resent changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { from } from 'rxjs';
+import{ FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormDispalyComponent } from './form-dispaly/form-dispaly.component';
 import { RepositoryComponent } from './repository/repository.component';
 import {HttpClientModule} from '@angular/common/http';
-import { NavBarComponent } from './nav-bar/nav-bar.component'
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchComponent } from './search/search.component'
 @NgModule({
   declarations: [
     AppComponent,
     FormDispalyComponent,
     RepositoryComponent,
-    NavBarComponent
+    NavBarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
